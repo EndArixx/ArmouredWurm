479e6a78327b17506436c42a13fb4843eb53ad9a
endarixx <HendArix@gmail.com>
2015-03-13 07:55:24 -0400

    LevelEditor
    John:
    Started working on a new class its just comments right now as 
    i have started designing what i want the lvl editor to look like.

---
+++ ArmouredWurm/src/game/LevelEditor.java
@@ -0,0 +1,10 @@
+package game;
+//this is a new idea i had to help make lvls
+
+/*Level Editor!
+ * Goals: to creates a fast and easy way to generate the level txt file that can easily be read in by the main game engine, 
+ * this is to help prevent types when creating the level and will also allow the people to see how the level will look visually.
+ */
+public class LevelEditor {
+
+}
