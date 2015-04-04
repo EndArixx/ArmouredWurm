dc99beea04fc7890afa5969f10aad3933175537b
endarixx <HendArix@gmail.com>
2015-04-04 00:09:06 -0400

    Back
    I will once again begin workin on Armoured Wurm. looks like the next big
    step will be animation and AI. once I have finished those...ITS TIME TO
    MAKE A GAME WOOHOOOOOOOOOOOOOOOOOOO

--- ArmouredWurm/src/game/Engine.java
+++ ArmouredWurm/src/game/Engine.java
@@ -58,7 +58,7 @@ public class Engine  extends Applet implements Runnable, KeyListener
 	private boolean isLoading = false;
 	private int loadTarget;
 		//For Testing hitboxes 
-	public final static boolean renderHitBox = false;
+	public final static boolean renderHitBox = true;
 	
 	
 		//Pause Menu Data
