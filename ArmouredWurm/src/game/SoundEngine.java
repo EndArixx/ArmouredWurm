e5c63fb2b8b80a30ae4f261fbf7df11bd6c61ebc
endarixx <HendArix@gmail.com>
2015-05-08 14:45:47 -0400

    SoundEngine
    John:
    Its time to add sound to the game. this will be done using a SoundEnine.

---
+++ ArmouredWurm/src/game/SoundEngine.java
@@ -0,0 +1,21 @@
+package game;
+/*
+ * This will be the sound Class, it will have a queue that the Engine sends sounds to, 
+ * this classes job will be to look at the queue and if there is a sound in the queue. play this sound.
+ * it will also have volume control and other such things. 
+ * 
+ *  Ideas:
+ *  Map ( String -> sound)
+ *  whens the engine starts it will load all the sounds to a map 
+ *  
+ *  then when something wants to make a sound they will send a STring over through the queue. 
+ *  the sound will look at the String find the appropriate sound in the Map and play it.
+ *  
+ *  text file with all the availible sounds?
+ *  
+ *  Level music on loop?
+ */
+public class SoundEngine 
+{
+
+}
