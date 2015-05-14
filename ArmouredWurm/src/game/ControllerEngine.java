657fdf3aa3422dcc10d7df49e179411db68aa266
endarixx <HendArix@gmail.com>
2015-05-14 14:07:44 -0400

    Controller
    John: 
    starting work on controller support.

---
+++ ArmouredWurm/src/game/ControllerEngine.java
@@ -0,0 +1,18 @@
+package game;
+/*
+ * This will be the controller zone.
+ * 
+ * I want to basically create a controller object and have it handle all its stuff separately.
+ * it will just relay control booleans to the main engine. it has no idea that its part of a game.
+ * 
+ */
+public class ControllerEngine {
+
+	
+	//MAIN<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>	
+		public static void main(String[] args) 
+		{
+			//Controller Test Zone
+		}
+	
+}
