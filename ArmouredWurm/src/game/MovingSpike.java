b71a2cef152f5858a9c9a087fefc40e8766b5029
endarixx <HendArix@gmail.com>
2015-09-19 20:39:35 -0400

    engine - starting to add the moving/destroyable spikes.
    John:
    Working on adding a new version of the spikes that can move and also
    will be destroyable. and i plan on making all platforms be able to both
    move and be destroyed. this will help make the new levels more
    interesting.

--- ArmouredWurm/src/game/MovingSpike.java
+++ ArmouredWurm/src/game/MovingSpike.java
@@ -2,21 +2,47 @@ package game;
 
 import java.awt.image.BufferedImage;
 import java.util.Map;
+import java.util.Queue;
 
-public class MovingSpike extends Spike {
+public class MovingSpike extends Spike 
+{
+	protected int leftPatrol, rightPatrol;
+	protected boolean FF;
 
 	public MovingSpike(String spriteloc, int x, int y,
-			Map<String, BufferedImage> spriteData) {
+			Map<String, BufferedImage> spriteData) 
+	{
 		super(spriteloc, x, y, spriteData);
-		// TODO Auto-generated constructor stub
+		this.leftPatrol = -1;
+		this.rightPatrol = -1;
+		this.FF = true;
+		
 	}
 
 	public MovingSpike(String inImage, int x, int y, int width, int height,
 			int rowN, int colN, int timerspeed, int damage,
-			Map<String, BufferedImage> spriteData) {
-		super(inImage, x, y, width, height, rowN, colN, timerspeed, damage,
-				spriteData);
-		// TODO Auto-generated constructor stub
+			Map<String, BufferedImage> spriteData) 
+	{
+		super(inImage, x, y, width, height, rowN, colN, timerspeed, damage,spriteData);
+		
+		
 	}
+	
+	public void update(World theWorld,Queue<DamageHitbox> damageQ)
+	{
+		super.update(theWorld);
+		
+		
+		
+		animateCol();
+	}
+	
+	public void setPath(int inLeft, int inRight)
+	{
+		this.leftPatrol = inLeft;
+		this.rightPatrol = inRight;
+	}
+	
+	
 
 }
--- ArmouredWurm/src/game/Platform.java
+++ ArmouredWurm/src/game/Platform.java
@@ -1,17 +1,26 @@
 package game;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 public class Platform extends Sprite
 {
 	protected int trueX, trueY, value;
 	protected double parallaxSpeed;
+	protected boolean destroyable,destroyed;
+	protected String destroyedSprite;
+	
 		//this is needed to prevent losing alot of data when moving parallax elements
 	protected double trueXdub, trueYdub;
 	public Platform(String spriteloc, int x, int y,  Map<String,BufferedImage> spriteData)
 	{
 		super(spriteloc,x,y, spriteData);
+		this.destroyable = false;
+		this.destroyed = false;
 		trueX = x;
 		trueY = y;
 		trueXdub = trueX;
@@ -23,6 +32,8 @@ public class Platform extends Sprite
 	public Platform(String inImage,int x,int y,int width ,int height,int rowN,int colN,int timerspeed,  Map<String,BufferedImage> spriteData) 
 	{
 		super(inImage,x,y,width,height,rowN, colN,timerspeed,  spriteData);
+		this.destroyable = false;
+		this.destroyed = false;
 		trueX = x;
 		trueY = y;
 		trueXdub = trueX;
@@ -34,6 +45,8 @@ public class Platform extends Sprite
 	public Platform(String inImage,int x,int y,int width ,int height,int ColN,int timerspeed,  Map<String,BufferedImage> spriteData,int value)
 	{
 		super(inImage,x,y,width,height,0,ColN,timerspeed,spriteData);
+		this.destroyable = false;
+		this.destroyed = false;
 		trueX = x;
 		trueY = y;
 		trueXdub = trueX;
@@ -64,6 +77,34 @@ public class Platform extends Sprite
 		this.trueY = -height;
 		this.col = 0;
 	}
+	public void make_Destroyable(String destroyedloc, Map<String,BufferedImage> spriteData )
+	{
+			//John New code 
+		BufferedImage spriteMap = null;
+		try 
+		{
+			if(new File(destroyedloc).isFile())
+			{
+				spriteMap = ImageIO.read(new File(destroyedloc));
+
+			}
+			else
+			{
+				spriteMap = ImageIO.read(getClass().getResource("/"+destroyedloc));
+			}
+			this.destroyable = true;
+			this.destroyed = false;
+			this.destroyedSprite = destroyedloc;
+			spriteData.put(destroyedloc,spriteMap);
+		}
+		catch (IOException e) 
+		{
+			this.destroyable = false;
+			System.out.println("Error, Bad Sprite:"+ destroyedloc);
+			//e.printStackTrace();
+		}
+	}
+	
 	public int getTrueX()
 		{return this.trueX;}
 	public int getTrueY()
