1a4c9e637bd2a6dda66eb20a2ea647049a30d178
endarixx <HendArix@gmail.com>
2015-09-19 20:18:13 -0400

    minor changes

--- ArmouredWurm/src/game/MovingSpike.java
+++ ArmouredWurm/src/game/MovingSpike.java
@@ -5,12 +5,18 @@ import java.util.Map;
 
 public class MovingSpike extends Spike {
 
+	public MovingSpike(String spriteloc, int x, int y,
+			Map<String, BufferedImage> spriteData) {
+		super(spriteloc, x, y, spriteData);
+		// TODO Auto-generated constructor stub
+	}
+
 	public MovingSpike(String inImage, int x, int y, int width, int height,
 			int rowN, int colN, int timerspeed, int damage,
-			Map<String, BufferedImage> spriteData) 
-	{
-		super(inImage, x, y, width, height, rowN, colN, timerspeed, damage, spriteData);
-		
+			Map<String, BufferedImage> spriteData) {
+		super(inImage, x, y, width, height, rowN, colN, timerspeed, damage,
+				spriteData);
+		// TODO Auto-generated constructor stub
 	}
 
 }
