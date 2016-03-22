314db6a1ec118e997703ef7eea81092547e16836
endarixx <HendArix@gmail.com>
2016-03-21 22:00:56 -0400

    Engine - states
    John:
    working on states, created constructors for them.
    
    
    V 1.0.205

--- ArmouredWurm/src/game/triggers/State.java
+++ ArmouredWurm/src/game/triggers/State.java
@@ -5,7 +5,7 @@ public class State
 /*
 This is nothing more then a boolean and a name 
 
-I don't know if i have to hardcode it
+I don't know if i have to hard code it
 
  
  Ok so there will be a string to State maps.
@@ -16,35 +16,40 @@ I don't know if i have to hardcode it
 	1) dead
 	2) moving
 	3) airborne
-		i) Jump
-		ii) fall
-		iii) fly?
+		i) Jumping
+		ii) falling
+		iii) flying
 	4) touching ground
-		i)platform
-		ii)ground
+		i)on platform
+		ii)on ground
 	5) idle
 	6) attacking
 		i) Light
 		ii) Heavy
 		iii) Special
 	7) spot enemy
-	8) has aggro
+	
+	--MORE FOR LATER--
+	8) has aggro 
+	9) guard
+	10) croutch
+	11) swim
  
  states should have features, things that can apply to multiple states
 
-the trigger system will worry about the acutal names of 'allowed states' which is more then one
+the trigger system will worry about the actual names of 'allowed states' which is more then one
 but when you actually go to pick a state it should be super simpler
 AKA
-NAME types and possibe suptimes.
+NAME types and possible sub-types.
 
-do i allow 'idential states?'
+do i allow 'identical states?'
 sure why not?
-the trigger system is the one that assignis states.
+the trigger system is the one that assigns states.
 
  
  */
 	private String name;
-	private boolean on;
+	//private boolean on;
 	//types
 	private boolean dead;
 	private boolean moving;
@@ -52,26 +57,40 @@ the trigger system is the one that assignis states.
 	private boolean idle;
 	private boolean attacking;
 	private boolean spotEnemy;
-	private boolean agroo;
+	//private boolean agro;
 	private String[] subType;
+	//IF YOU ADD SOMETHING HERE ADD IT TO THE RETURN STATEMENT!
 	
-	
-	public State(String inName) 
+	public State(String inName,boolean inDead,boolean inMoving,boolean inTouchingGround,boolean inIdle,boolean inAttacking,boolean inSpotEnemy) 
 	{
 		this.name = inName;
-		this.on = false;
+		this.dead = inDead;
+		this.moving = inMoving;
+		this.touchingGround = inTouchingGround;
+		this.idle = inIdle;
+		this.attacking = inAttacking;
+		this.spotEnemy = inSpotEnemy;
+		
 	}
+	public State(String inName,boolean inDead,boolean inMoving,boolean inTouchingGround,boolean inIdle,boolean inAttacking,boolean inSpotEnemy,String[] inSubType) 
+	{
+		this( inName, inDead, inMoving, inTouchingGround, inIdle, inAttacking, inSpotEnemy);
+		this.subType = inSubType;
+	}
+	public boolean[] getTypes()
+	{
+		boolean[] out = {dead,moving,touchingGround,idle,attacking,spotEnemy};
+		return out;
+	}
+	public String[] getSubTypes()
+	{
+		return subType;
+	}
+	
 	public String getName()
 	{
 		return name;
 	}
-	public boolean isOn()
-	{
-		return on;
-	}
-	public void setOn(Boolean in)
-	{
-		on = in;
-	}
+
 	
 }
