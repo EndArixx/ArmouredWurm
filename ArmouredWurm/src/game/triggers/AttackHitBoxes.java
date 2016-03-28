78d8d02e353be5f2db2386f80cfa144a4ef73477
endarixx <HendArix@gmail.com>
2016-03-28 19:01:52 -0400

    Engine - Hitboxes
    John:
    added a method to get hitboxes with an input x,y 
    
    V 1.0.208

--- ArmouredWurm/src/game/triggers/AttackHitBoxes.java
+++ ArmouredWurm/src/game/triggers/AttackHitBoxes.java
@@ -5,44 +5,51 @@ public class AttackHitBoxes
 	//This will be used for hitbox generation with the new trigger system
 	//This will be used at the same time as a combat Spark.
 	//this will prolly  need more then 4 things...
+	//This system will be Index[x,y,width,height]
+	//the X and Y are offsets
 	
-	private int[][] interanlHitboxes;
+	private int[][] internalHitboxes;
 		//a normal hitbox is only for points...but this might need an offset
 	private int lenofData = 4;
 	public AttackHitBoxes(int[][] in) 
 	{
-		interanlHitboxes = in;
+		internalHitboxes = in;
 	}
 	public AttackHitBoxes(String in)
 	{
 		String[] hitboxes = in.split(";");
 		String[] hit;
-		interanlHitboxes = new int[hitboxes.length][lenofData];
+		internalHitboxes = new int[hitboxes.length][lenofData];
 		for(int i = 0; i < (hitboxes.length); i++)
 		{
 			hit = hitboxes[i].split(",");
 			
 			for (int j = 0; j< lenofData; j++)
 			{
-				interanlHitboxes[i][j] = Integer.parseInt(hit[j]);
+				internalHitboxes[i][j] = Integer.parseInt(hit[j]);
 			}
 		}
 		
 	}
 	public int[] getHitBox(int index)
 	{	
-		return interanlHitboxes[index];
+		return internalHitboxes[index];
+	}
+	public int[] getHitBox(int index, int x, int y)
+	{
+		int[] out =  {internalHitboxes[index][0]+x,internalHitboxes[index][1]+y,internalHitboxes[index][2],internalHitboxes[index][3]};
+		return out;
 	}
 	
 	//this is for testing and can be removed afterword.
 	public String getString()
 	{
 		String out = "";
-		for(int i = 0;i < interanlHitboxes.length; i++)
+		for(int i = 0;i < internalHitboxes.length; i++)
 		{
 			for (int j = 0; j < lenofData; j++)
 			{
-				out += interanlHitboxes[i][j];
+				out += internalHitboxes[i][j];
 			}
 			out += " ";
 		}
