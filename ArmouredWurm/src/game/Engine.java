041dfc9f849533053a02f45592bc9499dbeec290
EndArixx <hendarix@gmail.com>
2016-06-26 16:36:19 -0400

    more tests trying to get the correct account stuff set up.
    V 1.0.221

--- ArmouredWurm/src/game/Engine.java
+++ ArmouredWurm/src/game/Engine.java
@@ -48,7 +48,7 @@ import javax.swing.JPanel;
 public class Engine  extends Applet implements Runnable, KeyListener 
 {
 
-	public String version = "Version 1.0.220";
+	public String version = "Version 1.0.221";
 		//For Testing hitboxes 
 	public final static boolean renderHitBox = false;
 	public boolean isEngine;
