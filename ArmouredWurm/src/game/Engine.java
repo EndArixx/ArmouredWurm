558c37e0cc0d634cb496752e10a5548ab9b381aa
EndArixx <hendarix@gmail.com>
2016-08-01 19:38:06 -0400

    Engine - trigger - sprite fix
    John:
    
    Sprite map was broken i had to fix serveral thing. but now all the
    animations are correctly mapped to a name and animation length, next
    step is to handle player input :)
    
    V 1.0.229

--- ArmouredWurm/src/game/Engine.java
+++ ArmouredWurm/src/game/Engine.java
@@ -48,7 +48,7 @@ import javax.swing.JPanel;
 public class Engine  extends Applet implements Runnable, KeyListener 
 {
 
-	public String version = "Version 1.0.227";
+	public String version = "Version 1.0.229";
 		//For Testing hitboxes 
 	public final static boolean renderHitBox = false;
 	public boolean isEngine;
