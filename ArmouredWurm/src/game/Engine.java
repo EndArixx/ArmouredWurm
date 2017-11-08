aa1675bc39e64e64b9c96194fb8b9bd07d7e83a5
EndArixx <hendarix@gmail.com>
2017-11-07 19:07:52 -0500

    John:
    Added some more stuff to the Actor still trying to figure out how I want
    to do this.
    
    V 1.1.305

--- ArmouredWurm/src/game/Engine.java
+++ ArmouredWurm/src/game/Engine.java
@@ -41,7 +41,7 @@ import javax.swing.JPanel;
 public class Engine  extends Applet implements Runnable, KeyListener 
 {
 
-	public String version = "Version 1.1.304";
+	public String version = "Version 1.1.305";
 		//For Testing hitboxes 
 	public final static boolean renderHitBox = false;
 		//ConsoleCrap
--- ArmouredWurm/src/game/actions/Actor.java
+++ ArmouredWurm/src/game/actions/Actor.java
@@ -1,7 +1,20 @@
 package game.actions;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import game.Tools;
+import game.triggers.AttackHitBoxes;
+import game.triggers.Spark;
+import game.triggers.State;
+import game.triggers.Trigger;
+import game.triggers.Value;
 
 
 /*
@@ -22,9 +35,77 @@ import java.util.Map;
 
 public class Actor extends game.Platform
 {		
-	public Actor(String spriteloc, int x, int y, Map<String, BufferedImage> spriteData) 
-	{
-		super(spriteloc, x, y, spriteData);
-	}
+	private String name;
+	protected String valueloc;
 	
+	public Actor(String indata,String spriteloc, int x, int y, Map<String, BufferedImage> spriteData) 
+	{
+		super(spriteloc,x,y,spriteData);
+		
+		this.name = indata.split(",")[0];
+		this.animate = true;
+		this.firstloop = true;
+		String[] temp = indata.split(indata);
+
+		
+		BufferedReader br = null;
+		int err = 0;
+		
+		//first you need to get the info parsed out, then you will call actor files to get the action data
+		//so what does an actor need?
+		// X,y (in super)
+		// True X True Y (this is in platform)
+		// actions
+		// causes/triggers
+		// animation data?
+		// values
+		
+		// How will the data work?
+		/*
+		  	Map data:
+		  		ActorName, X, Y, ActorType, 
+		  			other? patrol, speficic map related things?
+		  	ActorType File
+		  		actions
+		  		values
+	 
+		  
+		 */
+
+		try 
+		{
+			//create a new Armoured Wurm reader.
+			Tools reader = new Tools();
+			
+	    	//Favor external files, then internal
+	    	err= 1;
+	    	//br = reader.getBR(infile);
+	        String line = Tools.readlineadv(br);
+	    	//reader.closeBR();
+	    	
+	    	
+	    	//Values---------------------------------------------------------
+	        /*
+	        err= 7;
+	    	br = reader.getBR(valueloc);
+	        
+	        this.valueMap = new HashMap<String,Value>();
+	        Value tValue;
+	        while((line = Tools.readlineadv(br))!=null)
+	        {
+	        	tValue = new Value(line);
+		        this.valueMap.put(tValue.getName(),tValue);
+	        }
+	        reader.closeBR();
+	        */
+	        
+	        
+	    } catch (Exception e) 
+		{
+	    	System.out.println("Im sorry the Actor: "+name+" or one of its files could not be loaded!\n Error #"+ err + "\n " + e.toString());
+	    	//JOptionPane.showMessageDialog(null,  "Player File: "+infile +"Error:"+err+"\n"+ e.toString() , "Render Error", JOptionPane.ERROR_MESSAGE);
+	    } 
+		
+		
+	}
 }
